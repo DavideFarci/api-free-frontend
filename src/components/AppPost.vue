9d6abe91fa23e86df164cf31a2c439d86a260c1d
DavideFarci <yank92.df@gmail.com>
2023-11-28 12:35:59 +0100

    add post list and post component

---
+++ src/components/AppPost.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    postData: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <h3>{{ postData.title }}</h3>
+    <p>{{ postData.content }}</p>
+  </div>
+</template>
+
+<style></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,32 @@
 <script>
-export default {};
+import axios from "axios";
+import AppPost from "../components/AppPost.vue";
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    async getPosts() {
+      this.posts = (await axios.get("http://localhost:5174/posts")).data;
+      console.log(this.posts.data);
+    },
+  },
+  created() {
+    this.getPosts();
+  },
+  components: { AppPost },
+};
 </script>
 
 <template>
-  <h1>Sono la Home</h1>
+  <div class="container mx-auto py-8">
+    <h1 class="text-center text-4xl font-bold">Benvenuto nel mio blog</h1>
+    <div class="postList">
+      <AppPost v-for="post in posts.data" :key="post.id" :postData="post" />
+    </div>
+  </div>
 </template>
 
 <style></style>
